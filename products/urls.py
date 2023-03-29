05b16b7d6322c4e213fbd889c4045462fa62b687
Hasan Atki <djamblers@gmail.com>
2023-03-29 19:23:59 +0000

    Rename app products

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/', views.products, name='products'),
+    path('artworks/', views.artworks_view, name='artworks'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,13 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Artwork, Creator
 
 # Create your views here.
 
 
-def products(request):
+def artworks_view(request):
+    """
+    Rendering all products
+    """
     artworks = Artwork.objects.all()
     creators = Creator.objects.all()
     return render(request, 'products.html', {
         'artworks': artworks,
         'creators': creators
         })
+
+
+def artwork_view(request, artwork_id):
+    artwork = get_object_or_404(Artwork, pk=artwork_id)
+    return render(request, 'artwork.html', {'artwork': artwork})
