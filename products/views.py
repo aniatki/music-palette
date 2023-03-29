700eadf9ef9278f578bc64382a345ddf1b37f908
Hasan Atki <djamblers@gmail.com>
2023-03-29 22:06:17 +0000

    Implement functionality for search queries and filtering artworks by name or description

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.db.models import Q
 from .models import Artwork, Creator
+from django.contrib import messages
 
 # Create your views here.
 
@@ -10,9 +12,20 @@ def products_view(request):
     """
     artworks = Artwork.objects.all()
     creators = Creator.objects.all()
+    query = None
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, 'No criteria entered to search')
+                return redirect(reverse('products'))
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            artworks = artworks.filter(queries)
+
     context = {
         'artworks': artworks,
-        'creators': creators
+        'creators': creators,
+        'search_query': query
     }
     return render(request, 'products.html', context)
 
