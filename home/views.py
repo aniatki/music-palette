17f6dfb02427576841475b2c9708c469ee373f50
Hasan Atki <djamblers@gmail.com>
2023-03-29 15:20:51 +0000

    Fix template in index.html for displaying the correct format of featured products for sale

--- home/views.py
+++ home/views.py
@@ -1,6 +1,21 @@
+"""
+Views module serving the index page
+"""
+
+
 from django.shortcuts import render
+from products.models import Artwork, Creator
 
 # Create your views here.
 
+
 def index(request):
-    return render(request, 'home/index.html')
+    """
+    Serving the home view
+    """
+    artworks = Artwork.objects.all()
+    creators = Creator.objects.all()
+    return render(request, 'home/index.html', {
+        'artworks': artworks,
+        'creators': creators
+        })
