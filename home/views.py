e48d491fda370005ecf15f8c1b764df1c7d70eaf
Hasan Atki <djamblers@gmail.com>
2023-03-29 19:24:03 +0000

    Revert "Fix template in index.html for displaying the correct format of featured products for sale"
    This reverts commit 17f6dfb02427576841475b2c9708c469ee373f50.

--- home/views.py
+++ home/views.py
@@ -1,21 +1,6 @@
-"""
-Views module serving the index page
-"""
-
-
 from django.shortcuts import render
-from products.models import Artwork, Creator
 
 # Create your views here.
 
-
 def index(request):
-    """
-    Serving the home view
-    """
-    artworks = Artwork.objects.all()
-    creators = Creator.objects.all()
-    return render(request, 'home/index.html', {
-        'artworks': artworks,
-        'creators': creators
-        })
+    return render(request, 'home/index.html')
